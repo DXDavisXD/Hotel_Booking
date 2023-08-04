bbc1cbaa98b1aadb1a1687e73c75beef090521b7
DXDavisXD <127974919+DXDavisXD@users.noreply.github.com>
2023-08-04 15:25:42 +0530

    Update Footer.jsx

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2022 Deep Saha.</div>
+      <div className="fText">Copyright © 2023 Deep Saha.</div>
     </div>
   );
 };
